b5e8baf7ee2c0fa2118acdcfdda10d8e147de277
Fred Matias <fredrmatias@gmail.com>
2023-03-31 16:16:22 +0100

    populate sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   const { isOpen, setIsOpen } = useContext(SidebarContext);
   return ( 
   <div>
-    <div>Header</div>
+    <div className='bg-pink-200'>Header</div>
     <div onClick={()=> setIsOpen(!isOpen)} className='cursor-pointer flex relative' ><BsBag className='text-2xdl'/></div>
   </div>
   );
